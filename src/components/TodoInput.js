fd75db8c8de59588448423d29d3dda4137709c48
muskaanpatell <muskaanpatell1012@gmail.com>
2025-02-16 02:52:26 +0530

    app.js file

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoInput() {
+  return (
+    <div className="input-container">
+      <input type="text" className="input-box-todo" />
+    </div>
+  );
+}
+
+export default TodoInput;
