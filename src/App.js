628e456e4b1afa75e49006b58323e7a6e5767457
muskaanpatell <muskaanpatell1012@gmail.com>
2025-03-06 17:24:32 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import TodoInput from "./components/TodoInput";
+//import TodoInput from "./components/TodoInput";
+import TodoInput from "./components/TodoInput"; // Correct import
+import TodoList from "./components/TodoList";
+
 function App() {
+  const [ListTodo, setListTodo] = useState([]);
+  let addList = (InputText) => {
+    if (InputText !== '')
+      setListTodo([...ListTodo, InputText]);
+  }
+  const deleteListItem = (key) => {
+    let newListTodo = [...ListTodo];
+    newListTodo.splice(key, 1)
+    setListTodo([...newListTodo])
+  }
+
   return (
     <div className="main-container">
       <div className="center-container">
-        <TodoInput />
+        <TodoInput addList={addList} />
+        <h1 className="app-heading">
+          TODO
+        </h1>
+        <hr />
+        {ListTodo.map((listItem, i) => {
+          return (
+            <TodoList key={i} index={i} item={listItem} deleteItem={deleteListItem} />
+          )
+        })}
       </div>
     </div>
   );
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,9 +1,35 @@
-import React from "react";
+//import use state and reat
+import React, { useState } from "react";
 
-function TodoInput() {
+function TodoInput(props) {
+  const [InputText, setInputText] = useState("");// declare state variable 
+  const handleEnterPress = (e) => {
+    if (e.keyCode === 13) {
+      props.addList(InputText)
+      setInputText('')
+    }
+  }
   return (
     <div className="input-container">
-      <input type="text" className="input-box-todo" />
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your todo"
+        value={InputText}
+        onChange={e => {
+          setInputText(e.target.value)
+
+        }}
+        onKeyDown={handleEnterPress}
+
+      />
+
+      <button className="add-btn"
+        onClick={() => {
+          props.addList(InputText)
+          setInputText('')
+        }}>+</button>
+      {/* <div>{InputText}</div> */}
     </div>
   );
 }
